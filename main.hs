39299c7d0f77f147c229f0854d5845f6e4218ec4
valenp2024 <167101999+valenp2024@users.noreply.github.com>
2025-09-30 19:34:22 -0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -12,13 +12,13 @@ abss x
 
 sumAll :: [Int] -> Int
 -- sumAll (x)
---      | null x = 0
+--      | null x = 0 -- esto es la otra de hacerlo, actúa como condicionales (son como if y else)
 --      | otherwise = head (x) + (sumAll.(tail)) (x)
 sumAll [] = 0
 sumAll (x:xs) = x + sumAll xs
 
 --14
-pow1 :: Int -> Int
+pow1 :: Int -> Int 
 pow1 d = d
 
 pow2 :: Int -> (Int -> Int)
@@ -30,3 +30,6 @@ pow3 d c e = d + c + e
 -- 15
 suma4 :: Int -> (Int -> Int)
 suma4 = pow3 4
+
+
+--16 
